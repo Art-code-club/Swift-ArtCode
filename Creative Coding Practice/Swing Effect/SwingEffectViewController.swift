a8c7965b3b705972ff29eeff70d10693b947000a
Fomagran <fomagran6@naver.com>
2021-11-22 12:39:54 +0900

    [add] UIView에 팬제스쳐 달기

--- Creative Coding Practice/Swing Effect/SwingEffectViewController.swift
+++ Creative Coding Practice/Swing Effect/SwingEffectViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class SwingEffectViewController: UIViewController {
 
+    //MARK:- Properties
+    
+//    var square:UIView = {
+//       let view = UIView()
+    //        return view
+    //    }()
+    
+    @IBOutlet weak var square: UIView!
+    
+    //MARK:- Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    //MARK:- Help Functions
+    
+    //MARK:- IBActions
+    
+    @IBAction func dragSquare(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: self.view) //
+        square.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        sender.setTranslation(.zero, in: self.view)
     }
 }
