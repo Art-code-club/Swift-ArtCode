d665c3cf655e8e406068321d5c23b9cdc8d44c93
Fomagran <fomagran6@naver.com>
2021-12-03 17:53:06 +0900

    [add] Wave 추가

--- ArtCode/Swift/Swift ArtCode/Wave/WaveViewController.swift
+++ ArtCode/Swift/Swift ArtCode/Wave/WaveViewController.swift
@@ -7,105 +7,87 @@
 
 import UIKit
 
+struct Point {
+    var x:CGFloat
+    var y:CGFloat
+    var max:CGFloat
+    var min:CGFloat
+    var addY:CGFloat
+    var center:CGPoint {
+        return CGPoint(x: x, y: y)
+    }
+}
+
+struct Line {
+    let layer:CAShapeLayer
+    let p1:Int
+    let p2:Int
+    var color:UIColor
+}
+
 class WaveViewController: UIViewController {
-    
-    let redDot1:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    let redDot2:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    let redDot3:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    let redDot4:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    let redDot5:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    let redDot6:UIView = {
-        let view:UIView = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
+
+    private var timer : Timer?
+    private var points:[Point] = []
+    private var h:CGFloat = 0
+    private var lineCount:Int = 3
+    private var dotCount:Int = 4
+    private let colors:[UIColor] = [.systemPink,.systemGreen,.systemRed,.systemBlue,.systemCyan,.systemMint,.systemTeal,.systemOrange,.systemYellow,.systemPurple]
+    private var lines:[Line] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout()
-        
-
-        
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-            self.moveRedDot(redDot: self.redDot2,duration: 0.8,move: 100)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-            self.moveRedDot(redDot: self.redDot3,duration: 0.8,move: 100)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.moveRedDot(redDot: self.redDot4,duration: 0.8,move: 100)
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.77) {
-            self.moveRedDot(redDot: self.redDot5,duration: 0.8,move: 100)
-        }
-    }
-
-    
-    func setLayout() {
-        let w = self.view.frame.width/5
-        redDot1.frame = CGRect(x:0, y:self.view.center.y, width: 10, height: 10)
-        redDot1.layer.cornerRadius = 5
-        self.view.addSubview(redDot1)
-        
-        redDot2.frame = CGRect(x:w*1, y:self.view.center.y, width: 10, height: 10)
-        redDot2.layer.cornerRadius = 5
-        self.view.addSubview(redDot2)
-        
-        redDot3.frame = CGRect(x:w*2, y:self.view.center.y, width: 10, height: 10)
-        redDot3.layer.cornerRadius = 5
-        self.view.addSubview(redDot3)
-        
-        redDot4.frame = CGRect(x:w*3, y:self.view.center.y, width: 10, height: 10)
-        redDot4.layer.cornerRadius = 5
-        self.view.addSubview(redDot4)
-        
-        redDot5.frame = CGRect(x:w*4, y:self.view.center.y, width: 10, height: 10)
-        redDot5.layer.cornerRadius = 5
-        self.view.addSubview(redDot5)
-        
-        redDot6.frame = CGRect(x:self.view.frame.width-10, y:self.view.center.y, width: 10, height: 10)
-        redDot6.layer.cornerRadius = 5
-        self.view.addSubview(redDot6)
+        configure()
     }
     
-    func moveRedDot(redDot:UIView,duration:CGFloat,move:CGFloat) {
-        UIView.animate(withDuration:duration) {
-            redDot.center.y = self.view.center.y + move
-        } completion: { _ in
-            UIView.animate(withDuration: duration) {
-                redDot.center.y = self.view.center.y - move
-            } completion: { _ in
-                self.moveRedDot(redDot:redDot,duration:duration,move: move)
+    //MARK:- Help Functions
+    
+    func configure() {
+        let w:CGFloat = self.view.frame.width/CGFloat(dotCount+1)
+        let center = self.view.center.y
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        h = self.view.center.y
+        for i in 1...dotCount {
+            let x:CGFloat = w*CGFloat(i)
+            let y:CGFloat = center
+            let max:CGFloat = CGFloat.random(in: 30...120)
+            let min:CGFloat = CGFloat.random(in: (-120)...(-30))
+            let addY:CGFloat = CGFloat(i%2) == 0 ? -1:1
+            points.append(Point(x: x, y: y, max: max, min: min, addY: addY))
+        }
+        
+        for _ in 0..<lineCount {
+            lines.append(Line(layer:CAShapeLayer(),p1: (0..<points.count).randomElement()!, p2: (0..<points.count).randomElement()!, color: (colors.randomElement()!)))
+        }
+    }
+    
+    private func addCurve(line:Line,p1:Int,p2:Int) {
+        line.layer.removeFromSuperlayer()
+        let height:CGFloat = self.view.frame.height
+        let width:CGFloat = self.view.frame.width
+        let linePath = UIBezierPath()
+        linePath.move(to:CGPoint(x: 0, y:height))
+        linePath.addLine(to:CGPoint(x: 0, y:height/2))
+        linePath.addCurve(to:CGPoint(x: width, y:height/2), controlPoint1:points[p1].center, controlPoint2:points[p2].center)
+        linePath.addLine(to: CGPoint(x: width, y: height))
+        line.layer.path = linePath.cgPath
+        line.layer.fillColor = line.color.withAlphaComponent(0.4).cgColor
+        view.layer.addSublayer(line.layer)
+    }
+    
+    //MARK:- @objc Functions
+    
+    @objc
+    func timerCallback() {
+        for i in 0..<points.count {
+            points[i].y += points[i].addY
+            if points[i].y > h + points[i].max  || points[i].y < h + points[i].min {
+                points[i].addY = -(points[i].addY)
             }
+        }
+        
+        for i in 0..<lineCount {
+            addCurve(line:lines[i],p1:lines[i].p1,p2:lines[i].p2)
         }
     }
 }
